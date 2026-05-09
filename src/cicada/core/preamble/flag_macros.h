39db26e9d6fc39d7b2acee188590339f9737545d
ahraman <ahraman.programming@gmail.com>
2026-05-09 23:50:18 +0330

    refactor: slight change to the preprocessor guard surrounding `CICADA_DEBUG` macro definition

--- src/cicada/core/preamble/flag_macros.h
+++ src/cicada/core/preamble/flag_macros.h
@@ -1,16 +1,15 @@
 #ifndef CICADA_CORE_PREAMBLE_FLAG_MACROS_H_
 # define CICADA_CORE_PREAMBLE_FLAG_MACROS_H_
 
-#  if !defined(CICADA_NO_DEBUG) && !defined(CICADA_DEBUG)
-#   if defined(_DEBUG) || defined(DEBUG)
+# if (!defined(CICADA_NO_DEBUG) && !defined(CICADA_DEBUG)) \
+    && (defined(_DEBUG) || defined(DEBUG))
 /// Macro `CICADA_DEBUG` is defined whenever the program is compiled in debug mode. This macro
 /// enables a number of debug-only checks, markers and profilers.
 ///
 /// Generation of this macro can be disabled by defining the macro `CICADA_NO_DEBUG` prior to
 /// including this header.
-#    define CICADA_DEBUG
-#   endif
-#  endif
+#  define CICADA_DEBUG
+# endif
 
 # if (!defined(CICADA_NO_PRAGMA_ONCE) && !defined(CICADA_PRAGMA_ONCE)) \
     && ((defined(_MSC_VER) && (_MSC_VER >= 1020))) // MSVC >= v4.2
