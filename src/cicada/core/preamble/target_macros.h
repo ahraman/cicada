c184e52a24c225707ef9fb0a76e524cd69c261a2
ahraman <ahraman.programming@gmail.com>
2026-05-09 23:28:27 +0330

    feat: add macros that detect the target OS, architecture, and compiler (currently only MSVC on Windows is supported)

---
+++ src/cicada/core/preamble/target_macros.h
@@ -0,0 +1,36 @@
+#ifndef CICADA_CORE_PREAMBLE_TARGET_MACROS_H_
+# define CICADA_CORE_PREAMBLE_TARGET_MACROS_H_
+
+# include "cicada/core/preamble/flag_macros.h"
+
+# ifdef CICADA_PRAGMA_ONCE
+#  pragma once
+# endif
+
+# ifdef _WIN32
+#  define CICADA_OS_WINDOWS 1
+#  define CICADA_OS_NAME    "windows"
+
+#  ifdef _WIN64
+#   define CICADA_ARCHITECTURE_X64 1
+#   define CICADA_ARCHITECTURE_NAME "x64"
+#  else
+#   define CICADA_ARCHITECTURE_X86 1
+#   define CICADA_ARCHITECTURE_NAME "x86"
+#  endif
+# else
+#   define CICADA_OS_UNKNOWN 1
+#   define CICADA_OS_NAME "unknown"
+
+#   define CICADA_ARCHITECTURE_UNKNOWN 1
+#   define CICADA_ARCHITECTURE_NAME "unknown"
+# endif
+
+# ifdef _MSC_VER
+#   define CICADA_COMPILER_MSVC 1
+#   define CICADA_COMPILER_NAME "msvc"
+# else
+#  define CICADA_COMPILER_NAME "unknown"
+# endif
+
+#endif
